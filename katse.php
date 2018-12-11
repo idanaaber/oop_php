9cf1119894a688a3ed4f3eac2e2cc0f3985f027d
idaNaaber <kevin.randmaa@khk.ee>
2018-12-11 10:40:45 +0200

    else if tingimuslause lisatud

--- katse.php
+++ katse.php
@@ -5,48 +5,67 @@
  * Date: 11.12.2018
  * Time: 11:08
  */
-// muutujate defineerimine
-// $muutujaNimi = väärtus;
-$lehePealkiri = 'Katse leht';
+//Muutujate defineerimine
+//$muutujaNimi = väärtus;
+$lehePealkiri = 'Katseleht';
 $sisuPealkiri = 'Muutujate defineerimine';
-// muutujate sisu testvaatamine
+//muutujate sisu test vaatamine
 var_dump($lehePealkiri);
-echo '<br>';
-var_dump($sisuPealkiri);
-// andmetüübid
+//andmetüübid
 $taisarv = 7;
 $reaalarv = -2.6;
-$tekst = 'Tere PHP';
-$toevaartusTrue = true;
-$toevaartusFalse = false;
-// väärtuse tüübi kontroll - gettype($muutujaNimi)
-// väärtuse tüübi teisendus - settype($muutujaNimi, 'tüüp')
-// väljastamine
-// ühendamine 'tekst katkeb '.$muutujaNimi.' tekst jätkub'
+$tekst = 'Tere, PHP';
+$tõeväärtusTrue = true;
+$tõeväärtusFalse = false;
+echo '<br>';
+var_dump($sisuPealkiri);
+//väärtuse tüübi kontroll gettype($muutujaNimi)
+//väärtuse tüübi teisendus settype($muutujaNimi, 'tüüp')
+//Näide teksti väljastamisest ekraanile veebiprauseris
+//Ühendamine toimub 'tekst katkeb '.$muutujaNimi.' tekst jätkub'
+//tingimuslause
+/*
+ * if (tingimus == true) {
+ *         siis toimub see osa
+ * }else {
+ *         muidu toimub see osa
+ * }
+ * */
+$naitaSisu = true;
+$naitaPeidetudSisu = true;
 echo '
-    <!doctype html>
+    <!DOCTYPE html>
     <html>
         <head>
-            <title>'.$lehePealkiri.'</title>
+            <title>' . $lehePealkiri . '</title>
         </head>
-        <body>
-            <h1>'.$sisuPealkiri.'</h1>
+        <body>';
+if($naitaSisu == false){
+    //sisu väljastamine
+    echo '  <h1>' . $sisuPealkiri . '</h1>
             <h3>Andmetüübid</h3>
             <ul>
-                <li>$taisarv = '.$taisarv.' - '.gettype($taisarv).'</li>
-                <li>$reaalarv = '.$reaalarv.' - '.gettype($reaalarv).'</li>
-                <li>$tekst = '.$tekst.' - '.gettype($tekst).'</li>
-                <li>$toevaartusTrue = '.$toevaartusTrue.' - '.gettype($toevaartusTrue).'</li>
-                <li>$toevaartusFalse = '.$toevaartusFalse.' - '.gettype($toevaartusFalse).'</li>
-            </ul>
-            ';
-// tüübi teisendamise katse
-echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
-echo '<br>';
-settype($taisarv, 'string');
-echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
-echo '<br>';
-echo gettype($taisarv);
+                <li>$taisarv = ' . $taisarv . ' - ' . gettype($taisarv) . '</li>
+                <li>$reaalarv = ' . $reaalarv . ' - ' . gettype($reaalarv) . '</li>
+                <li>$tekst = ' . $tekst . ' - ' . gettype($tekst) . '</li>
+                <li>$tõeväärtusTrue = ' . $tõeväärtusTrue . ' - ' . gettype($tõeväärtusTrue) . '</li>
+                <li>$tõeväärtusFalse = ' . $tõeväärtusFalse . ' - ' . gettype($tõeväärtusFalse) . '</li>
+            </ul>'
+    ;
+    //tüübi teisendamine
+    echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
+    echo '<br>';
+    settype($taisarv,'string');
+    echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
+    echo '<br>';
+    echo gettype($taisarv);
+}else if ($naitaSisu == 1){
+    echo '<br>';
+    echo 'See on tavaline peidetud info.';
+}else {
+    echo '<br>';
+    echo 'Sisu on peidetud!';
+}
 // html lehe lõpp
 echo '
         </body>
--- kontroll.php
+++ kontroll.php
@@ -19,21 +19,21 @@ b) Väljastatud muutujate väärtused koos sobiva teksitga html faili kujul
 
 //muutujate defineerimine
 $nimi = 'Kevin Randmaa';
-$kursuseTähis = 'KD18';
+$kursuseTahis = 'KD18';
 $kursuseNumber = '1.';
 $kooliEmail = 'kevin.randmaa@khk.ee';
 echo '
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="et">
     <head>
-        <meta charset="UTF-8" />
-        <title>KD18</title>
+       
+        <title>Kontroll</title>
     </head>
     <body>
-        <p>$nimi = '. $nimi. '</p>
-        <p>$kursuseTähis = '. $kursuseTähis. '</p>
-        <p>$kursuseNumber = '. $kursuseNumber. '</p>
-        <p>$kooliEmail = '. $kooliEmail. '</p>
+        <p>Minu nimi on '. $nimi. '</p>
+        <p>Minu kursuse tähis Tartu Kutsehariduskeskuses on '. $kursuseTahis. '</p>
+        <p>Õpin '. $kursuseNumber. ' kursusel.</p>
+        <p>Minu kooli e-posti aadress on '. $kooliEmail. '</p>
     </body>
     </html>
  ';
